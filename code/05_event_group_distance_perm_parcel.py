d4fe231d8e6c8e8a0ae8dd50c577ba20f81c0c7e
Yibei Chen <yibeichan@gmail.com>
2024-08-07 12:55:04 -0400

    get brain beh correlation

--- code/05_event_group_distance_perm_parcel.py
+++ code/05_event_group_distance_perm_parcel.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 import gc
 import pickle
 import numpy as np
-from utils.coflt import read_coflt_data, get_seg_coflt, permutation_test, fdr_correction
+from utils.preproc import get_roi_and_network_ids
+from utils.coflt import read_coflt_data, reconstruct_matrix, get_seg_coflt, permutation_test, fdr_correction
 
-def main(n_parcel: int, output_dir: str, behav_dir: str) -> None:
+def main(n_parcel: int, output_dir: str, behav_dir: str, parcellation_dir: str) -> None:
     """
     Read data, perform calculations, and save results.
 
@@ -14,6 +15,7 @@ def main(n_parcel: int, output_dir: str, behav_dir: str) -> None:
         n_parcel (int): Number of parcels.
         output_dir (str): Directory to save the results.
         behav_dir (str): Directory containing behavioral results.
+        parcellation_dir (str): Directory containing parcellation data.
 
     Returns:
         None
@@ -29,28 +31,39 @@ def main(n_parcel: int, output_dir: str, behav_dir: str) -> None:
         lines = f.readlines()
         boundaries = np.array([int(float(line.strip())) for line in lines], dtype=int)
 
-    # Segment matrices based on event boundaries
-    affair_seg = get_seg_coflt(boundaries, affair_coflt)
-    paranoia_seg = get_seg_coflt(boundaries, paranoia_coflt)
+    # Load parcellation data
+    parcellation = np.load(os.path.join(parcellation_dir, f"yan_kong17_{n_parcel}parcels.npz"))
+    _, _, node_idx_dict, _ = get_roi_and_network_ids(parcellation, n_parcel)
 
+    # Reconstruct matrices
+    node_mtx_affair, _ = reconstruct_matrix(affair_coflt, node_idx_dict)
+    node_mtx_paranoia, _ = reconstruct_matrix(paranoia_coflt, node_idx_dict)
+
+    # Clean up memory
     del affair_coflt, paranoia_coflt
     gc.collect()
 
+    # Segment matrices based on event boundaries
+    affair_seg = get_seg_coflt(boundaries, node_mtx_affair)
+    paranoia_seg = get_seg_coflt(boundaries, node_mtx_paranoia)
+
     # Perform permutation test
-    observed, null_distribution, p_values = permutation_test(affair_seg, paranoia_seg, n_permutations=10000, n_jobs=4)
+    observed, null_distribution, p_values = permutation_test(affair_seg, paranoia_seg, n_permutations=5000, n_jobs=8)
     corrected_p_values = fdr_correction(p_values)
     
-     # Save results to .npz file
+    # Save results to .npz file
     distance_output_dir = os.path.join(output_dir, "group_distance", f"{n_parcel}parcel")
-    output_file = os.path.join(distance_output_dir, f"event_group_distance_perm_parcel.npz")
+    output_file = os.path.join(distance_output_dir, f"event_group_distance_perm_roi.npz")
     np.savez(output_file, observed=observed, null_distribution=null_distribution, p_values=p_values, corrected_p_values=corrected_p_values)
     print(f"Saved permutation test results for {n_parcel} parcels")
 
 if __name__ == "__main__":
     load_dotenv()
-    n_parcel = int(sys.argv[1])
+    # n_parcel = int(sys.argv[1])
+    n_parcel = 400
     scratch_dir = os.getenv("SCRATCH_DIR")
     behav_dir = os.path.join(scratch_dir, "output", "behav_results")
-    output_dir = os.path.join(output_dir, "output", "group_distance", f"{n_parcel}parcel")
+    parcellation_dir = os.path.join(scratch_dir, "data", "fmriprep", "yan_parcellations")
+    output_dir = os.path.join(scratch_dir, "output")
 
-    main(n_parcel, output_dir, behav_dir)
+    main(n_parcel, output_dir, behav_dir, parcellation_dir)
---
+++ code/07_brain_beh_corr_ap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=brain_beh_corr_ap
+#SBATCH --output=../logs/brain_beh_corr_ap_%j.out
+#SBATCH --error=../logs/brain_beh_corr_ap_%j.err
+#SBATCH --array=0
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=18G
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=yibei@mit.edu
+
+# Load necessary modules
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+conda activate prettymouth2
+
+# n_parcels=(500 600 700 800 900 1000)
+n_parcels=(400)
+
+n_parcel=${n_parcels[$SLURM_ARRAY_TASK_ID]}
+
+echo "n_parcel: $n_parcel"
+
+python 07_brain_beh_corr_ap.py $n_parcel
